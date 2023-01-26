c753ba36e0001ea64087ae0db06ef7852c6f3a7f
BARREL2 <barrelc12@gmail.com>
2023-01-26 23:52:10 +0900

    色々追加

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,100 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.BT_ButtonA = new System.Windows.Forms.Button();
+            this.TB_Box1 = new System.Windows.Forms.TextBox();
+            this.LB_ToUpper = new System.Windows.Forms.Label();
+            this.LB_ToLower = new System.Windows.Forms.Label();
+            this.LB_Timer = new System.Windows.Forms.Label();
+            this.LB_Counter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // BT_ButtonA
+            // 
+            this.BT_ButtonA.Location = new System.Drawing.Point(172, 12);
+            this.BT_ButtonA.Name = "BT_ButtonA";
+            this.BT_ButtonA.Size = new System.Drawing.Size(75, 23);
+            this.BT_ButtonA.TabIndex = 1;
+            this.BT_ButtonA.Text = "String";
+            this.BT_ButtonA.UseVisualStyleBackColor = true;
+            this.BT_ButtonA.Click += new System.EventHandler(this.BT_ButtonA_Click);
+            // 
+            // TB_Box1
+            // 
+            this.TB_Box1.Location = new System.Drawing.Point(12, 12);
+            this.TB_Box1.Name = "TB_Box1";
+            this.TB_Box1.Size = new System.Drawing.Size(154, 23);
+            this.TB_Box1.TabIndex = 2;
+            // 
+            // LB_ToUpper
+            // 
+            this.LB_ToUpper.AutoSize = true;
+            this.LB_ToUpper.Location = new System.Drawing.Point(12, 38);
+            this.LB_ToUpper.Name = "LB_ToUpper";
+            this.LB_ToUpper.Size = new System.Drawing.Size(38, 15);
+            this.LB_ToUpper.TabIndex = 3;
+            this.LB_ToUpper.Text = "label1";
+            // 
+            // LB_ToLower
+            // 
+            this.LB_ToLower.AutoSize = true;
+            this.LB_ToLower.Location = new System.Drawing.Point(12, 53);
+            this.LB_ToLower.Name = "LB_ToLower";
+            this.LB_ToLower.Size = new System.Drawing.Size(38, 15);
+            this.LB_ToLower.TabIndex = 4;
+            this.LB_ToLower.Text = "label2";
+            // 
+            // LB_Timer
+            // 
+            this.LB_Timer.AutoSize = true;
+            this.LB_Timer.Location = new System.Drawing.Point(172, 53);
+            this.LB_Timer.Name = "LB_Timer";
+            this.LB_Timer.Size = new System.Drawing.Size(34, 15);
+            this.LB_Timer.TabIndex = 5;
+            this.LB_Timer.Text = "timer";
+            // 
+            // LB_Counter
+            // 
+            this.LB_Counter.AutoSize = true;
+            this.LB_Counter.Location = new System.Drawing.Point(172, 38);
+            this.LB_Counter.Name = "LB_Counter";
+            this.LB_Counter.Size = new System.Drawing.Size(48, 15);
+            this.LB_Counter.TabIndex = 6;
+            this.LB_Counter.Text = "counter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(257, 93);
+            this.Controls.Add(this.LB_Counter);
+            this.Controls.Add(this.LB_Timer);
+            this.Controls.Add(this.LB_ToLower);
+            this.Controls.Add(this.LB_ToUpper);
+            this.Controls.Add(this.TB_Box1);
+            this.Controls.Add(this.BT_ButtonA);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private Button BT_ButtonA;
+        private TextBox TB_Box1;
+        private Label LB_ToUpper;
+        private Label LB_ToLower;
+        private Label LB_Timer;
+        private Label LB_Counter;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,85 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        Stopwatch sw;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             sw = new Stopwatch();
+        }
+
+        delegate void DeligProcess();
+        delegate void DeligProcess1();
+        private void CounterUpdate()
+        {
+            DeligProcess process = new DeligProcess(Counter);
+            this.Invoke(process);
+            return;
+        }
+        private void TimerUpdate()
+        {
+            DeligProcess1 process = new DeligProcess1(CTimer);
+            this.Invoke(process);
+            return;
+        }
+        private void BT_ButtonA_Click(object sender, EventArgs e)
+        {
+            LB_ToUpper.Text = TB_Box1.Text.ToUpper();
+            LB_ToLower.Text = TB_Box1.Text.ToLower();
+            BT_ButtonA.Enabled = false;
+
+            Task task = Task.Run(() =>
+            {
+
+                CounterUpdate();
+            });
+
+            Task task1 = Task.Run(() =>
+            {
+                TimerUpdate();
+            });
+        }
+
+        public async void Counter()
+        {
+            int counterValue;
+            try
+            {
+                counterValue = Convert.ToInt32(TB_Box1.Text);
+            }
+            catch(Exception ex)
+            {
+                counterValue = 50;
+            }
+            for (int i = 0; i < counterValue; i++)
+            {
+                await Task.Delay(100);
+                LB_Counter.Text = i.ToString();
+            }
+            TB_Box1.Text = "Counter end";
+            BT_ButtonA.Enabled = true;
+            sw.Stop();
+            LB_Timer.Text = sw.Elapsed.ToString();
+        }
+
+        public async void CTimer()
+        {   
+            if(LB_Timer.Text.Equals("timer"))
+            {
+                sw.Start();
+            }
+            else
+            {
+                sw.Restart();
+            }
         }
     }
 }
